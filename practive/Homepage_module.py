a2565251ca87fc8fb185ea664608e1fdd94bb7ce
ye-seong <dptjd0109@gmail.com>
2025-02-11 17:29:35 +0900

    ㄴㅇㄹ

--- practive/Homepage_module.py
+++ practive/Homepage_module.py
@@ -1,3 +1,8 @@
+class Admin:
+    admin_pass = '1234'
+    admin_value = True
+    error_num = 0
+
 class User:
     name = ''
     id = ''
@@ -7,5 +12,28 @@ class User:
         self.name = name
         self.id = id
         self.password = password
+
+class Post:
+    post_writer = ''
+    post_title = ''
+    post_detail = ''
+    post_reply = []
+
+    def __int__(self, writer, title, detail, reply):
+        self.post_writer = writer
+        self.post_title = title
+        self.post_detail = detail
+        self.post_reply = reply
+
+class Reply:
+    reply_writer = ''
+    reply_detail = ''
+
+    def __int__(self, writer, detail):
+        self.reply_writer = writer
+        self.reply_detail = detail
+
+        
+    
 
     
